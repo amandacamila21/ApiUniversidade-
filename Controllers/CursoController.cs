9e16ca7ebc0228f61e062317f7d17ba44f5cac25
amandacamila21 <bezerra.amanda@escolar.ifrn.edu.br>
2023-10-24 11:41:17 -0300

    sexto commit - get(id)

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -41,5 +41,17 @@ namespace apiUniversidade.Controllers
                 new {id = curso.Id}, 
                 curso);
         }
+
+        [HttpGet("{id:int}", Name="GetCurso")]
+        public ActionResult<Curso> Get(int id)
+        {
+            var curso = _context.Cursos.FirstOrDefault(p => p.Id == id);
+            if (curso is null)
+                return NotFound("Curso não encontrado.");
+
+            return curso;  
+        }  
+
+        
     }
 } 
